0070a0a1fd42c0a8f40897997d582e85c272efee
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-28 11:07:07 -0500

    Notificaciones: crear archivo desktop

---
+++ modulos/notificaciones/desktop/nt-desktop-send.js
@@ -0,0 +1,7 @@
+/* nt-desktop-send.js */
+(function initNtDesktopSend(global) {
+  "use strict";
+  const root = global.AgendaJeffModules = global.AgendaJeffModules || {};
+  const nt = root.Notificaciones = root.Notificaciones || {};
+  nt.Desktop = nt.Desktop || {};
+})(window);
