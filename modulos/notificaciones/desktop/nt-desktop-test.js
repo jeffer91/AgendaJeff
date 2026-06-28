31861b5b49bacb14db3a1b80d7c1bc9208f97200
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-28 11:07:52 -0500

    Notificaciones: crear archivo de pruebas

---
+++ modulos/notificaciones/desktop/nt-desktop-test.js
@@ -0,0 +1,7 @@
+/* nt-desktop-test.js */
+(function initNotificacionesDesktopTest(global) {
+  "use strict";
+  const root = global.AgendaJeffModules = global.AgendaJeffModules || {};
+  const nt = root.Notificaciones = root.Notificaciones || {};
+  nt.Desktop = nt.Desktop || {};
+})(window);
